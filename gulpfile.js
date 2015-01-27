22f7270534822e0cf34afdf31af2211ba1c26593
cipher <gmgateway@gmail.com>
2015-01-28 08:05:50 +0900

    Some tweaks
    Add marked and gulp-prettify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var less = require('gulp-less');
+var prettify = require('gulp-prettify');
 var rename = require('gulp-rename');
 var minify = require('gulp-csso');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var shell = require('gulp-shell');
 
 gulp.task('html', function() {
-  return gulp.src('index.jade')
-    .pipe(jade({
-      pretty: true
+  return gulp.src('pages/index.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('prettify', ['html'], function() {
+  return gulp.src('dist/*.html')
+    .pipe(prettify({
+      brace_style: 'collapse',
+      indent_size: 2,
+      indent_char: ' '
     }))
     .pipe(gulp.dest('./dist'));
 });
@@ -30,24 +40,23 @@ gulp.task('scripts', function() {
       'js/transition.js',
       'js/alert.js',
       'js/button.js',
-      'js/carousel.js',
       'js/collapse.js',
       'js/dropdown.js',
-      'js/modal.js',
       'js/tooltip.js',
-      'js/popover.js',
-      'js/scrollspy.js',
-      'js/tab.js',
-      'js/affix.js'
+      'js/tab.js'
     ])
-    .pipe(concat('bootstrap.min.js'))
+    .pipe(concat('bootstrap.tmp.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist/js'))
+    .pipe(shell([
+      'cat includes/bs-license.txt dist/js/bootstrap.tmp.js > dist/js/bootstrap.min.js',
+      'rm dist/js/bootstrap.tmp.js'
+    ]));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('*.jade', ['html']);
+  gulp.watch(['pages/*', 'includes/*.md'], ['prettify']);
   gulp.watch(['less/**'], ['less']);
 });
 
-gulp.task('default', ['html', 'less', 'scripts', 'watch']);
+gulp.task('default', ['prettify', 'less', 'scripts', 'watch']);
---
+++ includes/aside.md
@@ -0,0 +1,14 @@
+### Heading 3
+
+Recusandae ipsum distinctio omnis ut illum.
+
+* Geochemist
+* Financial manager
+* Commercial/residential surveyor
+* Solicitor, Scotland
+* Printmaker
+* Sports development officer
+* Teacher, special educational needs
+* Applications developer
+* Commercial/residential surveyor
+* Writer
